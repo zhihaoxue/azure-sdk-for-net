7ba3a032cabbdc753f39c987ea716a6ef751006a
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-10-21 20:31:58 -0700

    Add completed tutorial

--- sdk/api-learn/Azure.Learn.AppConfig/tests/LearnAppConfigTestEnvironment.cs
+++ sdk/api-learn/Azure.Learn.AppConfig/tests/LearnAppConfigTestEnvironment.cs
@@ -9,8 +9,15 @@ namespace Azure.Learn.AppConfig.Tests
     // To learn more about TestEnvironment classes, please see: https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/core/Azure.Core.TestFramework/README.md
     public class LearnAppConfigTestEnvironment : TestEnvironment
     {
+        // Note we use "api-learn" as the value for the serviceName
+        // parameter in the call to the base constructor. This should
+        // match the service directory name where your library lives.
         public LearnAppConfigTestEnvironment() : base("api-learn")
         {
         }
+
+        public string Endpoint => GetRecordedVariable("API-LEARN_ENDPOINT");
+        public string SettingKey => GetVariable("API-LEARN_SETTING_COLOR_KEY");
+        public string SettingValue => GetVariable("API-LEARN_SETTING_COLOR_VALUE");
     }
 }
